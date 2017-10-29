0c2e624306dc926ce179769be3bd69fe3ad7350a
Usagikeri <candidandoras@gmail.com>
2017-10-29 12:56:08 +0900

    add comment ver.1

--- janken.py
+++ janken.py
@@ -6,6 +6,8 @@ class Player:
     プレイヤーに関係するクラス
     クラス変数に以下の変数を持つ．
     name:名前
+    history:戦歴
+    now_hand:今出している手
     win:勝数
     lose:負け数
     draw:引き分け数
@@ -16,32 +18,35 @@ class Player:
     draw = 0
 
     """
-    初期化の関数．クラスがインスタンス化された時はまずこの関数が呼び出されます．
-    今回はselfとnを引数に渡しています．nに初期値John Doeを設定しているため引数がない場合はJohn Doeになる．
+    初期化の関数．クラスがインスタンス化された時はまずこの関数が呼び出される．
+    名前と戦歴を記録するhistoryリストの初期化を行う．
+
+    nに初期値John Doeを設定しているため引数がない場合はJohn Doeになる．
     クラスを呼び出す際にnを指定することでnameが設定される．
-    クラスの内にの変数に値を代入する場合は，
-    self.変数名
-    で指定する．
+    クラス内の変数に値を代入する場合は，
+    self.変数名 で指定する．
+
     e.g.)p1 = Player("Shibayama")
          p2 = Player()
 
          print(p1.name) -> Shibayama
          print(p2.name) -> John Doe
+
     """
     def __init__(self,n="John Doe"):
         self.name = n
         self.history = []
 
-
     """
     じゃんけんの手をだす関数（メソッド）
-    random.choiceでランダムで一文字生成
-    random.choice("012")なので文字型，
-    ranodm.choice([0,1,2])なら数値型で生成できる．
-    for文の時と同じ．文字列なら一文字，リストなら一要素として判定してランダムを作り出す．
+    random.randintでランダムで数字を生成
+    randintの引数に(0,2)を指定しているため，0から2の数字が生成される
+    勝敗を手の数字を計算して，判定するため乱数は数値型の方が望ましい．
 
-    handはクラス内の変数なのでselfが必要
-    rはクラス中の関数の変数なのでselfはいらない
+    random.choiceを使うのであれば，random.choice("012")ではなく，
+    ranodm.choice([0,1,2])とすることで数値型で生成できる．
+
+    for文の時と同じ．文字列なら一文字，リストなら一要素として判定してランダムを作り出すため．
     """
     def show_hand(self):
         r = random.randint(0,2)
@@ -51,18 +56,12 @@ class Player:
 
 class Judge:
     """
-    勝敗判定をするためのクラス
-    １対１のじゃんけんバトルだけではなく，複数人にも対応したい．
-    処理としてはインスタンスを引数にとり，クラス変数の勝敗の変数を加算することを想定している．
-    引数は可変長?
-
-    全員が同じ手，もしくは三種類の手ならばすべてのインスタンスのdrawフィールドに+1
-    二種類の場合，勝ちの手をだしたインスタンスのwinフィールドに+1，
-    負けの手を出したインスタンスのloseフィールドに+1
-    set()を使った重複の削除によって実装する予定
+    勝敗判定，結果の出力をするためのクラス
     """
 
-    # クラス内で使用する変数の宣言
+    """
+    クラス内で使用する変数の宣言
+    """
     hands = {0:"グー",
              1:"チョキ",
              2:"パー"}
@@ -71,20 +70,41 @@ class Judge:
         pass
 
     """
-    hand_listを使って勝敗を判断し，それをplayer_listのフィールドに加算する．
+    勝敗の判定を行うメソッド（関数）
+
+
+    全員が同じ手，もしくは三種類の手ならばすべてのインスタンスのdrawフィールドに+1
+    二種類の場合，勝ちの手をだしたインスタンスのwinフィールドに+1，
+    負けの手を出したインスタンスのloseフィールドに+1
     """
     def judge(self,player_list):
+        """
+        全員分の手を一つのリストに集める．
+        """
         hand_list = []
         for player in player_list:
             hand_list.append(player.now_hand)
 
+        """"
+        重複を除いた手のリストを作成する．
+        重複の削除には，set()を利用している．
+        setはリストではなく，集合を作る関数であるため，順番が保存されず，
+        ソートされ，リスト[0]から数が小さいものが順番に格納されている．
+        e.g.) hand_list = [2,2,1,1,1,0,2,0]
+              judgelist -> [0,1,2]
+        """
         judge_list = list(set(hand_list))
 
+        """
+        judge_listの要素数が1もしくは3．つまり全員が同じ手を出してもしくは，全員が違う手を出した場合に，全Playerのdraw変数に1を加算する．
+        そしてhistoryには，出した手と，勝敗結果を記録する．
+        """
         if len(judge_list) == 1 or len(judge_list) == 3:
             #全部にdrawを追加する処理
             for player in player_list:
                 player.draw += 1
                 player.history.append("{0}:draw".format(self.hands[judge_list[0]]))
+
 
         elif judge_list[0] == 0 and (judge_list[0] - judge_list[1]) == -1:
             for player in player_list:
